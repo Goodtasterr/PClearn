42dcb66686f3b24d3dd3a7f11c8177ea40cf6422
Goodtasterr <1261336144@qq.com>
2020-07-14 15:21:01 +0800

    add raildataloader

---
+++ RailDataLoader.py
@@ -0,0 +1,59 @@
+import os
+from torch.utils.data import Dataset
+import numpy as np
+import torch
+
+def pc_normalize(pc):
+    centroid = np.mean(pc, axis=0)
+    pc = pc - centroid
+    m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
+    pc = pc / m
+    return pc
+
+class RailNormalDataset(Dataset):
+    def __init__(self,root='/home/hwq/dataset/labeled/datanpy',npoints=25000,split='train'):
+        self.root = root
+        self.npoints = npoints
+
+        fns = sorted(os.listdir(root))
+        train_index = np.load('/home/hwq/dataset/labeled/trainindex.npy')
+        test_index = np.load('/home/hwq/dataset/labeled/testindex.npy')
+
+        alldatapath=[]
+        for fn in fns:
+            alldatapath.append(os.path.join(root,fn))
+
+        self.datapath=[]
+        if split == 'train':
+            for index in train_index:
+                self.datapath.append(alldatapath[index])
+        if split == 'test':
+            for index in test_index:
+                self.datapath.append(alldatapath[index])
+
+
+    def __getitem__(self, item):
+        fn = self.datapath[item]
+        data = np.load(fn).astype(np.float32)
+        point_set = data[:,:3]
+        seg = data[:, -1].astype(np.int32)
+
+        point_set = pc_normalize(point_set)
+        choice = np.random.choice(len(seg), self.npoints, replace=True)
+        point_set = point_set[choice]
+        seg = seg[choice]
+        return point_set, seg
+
+    def __len__(self):
+        return len(self.datapath)
+
+if __name__ == '__main__':
+    bs =5
+    train_dataset = RailNormalDataset(split='train')
+    test_dataset = RailNormalDataset(split='test')
+    train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size=bs)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset,batch_size=bs)
+
+    for point_set, seg_num in test_dataloader:
+        print((point_set)[0,:2,:1]*1e6)
+        exit()
--- aa.py
+++ aa.py
@@ -17,7 +17,7 @@ def pc_reduce(points,range):
     index_z = (points[:, 2] > (range[4])) & (points[:, 2] < range[5])
     index = index_x & index_y & index_z
     index2 = np.asarray(index).astype(int)
-    print(index2)
+    # print(index2)
     points_new = points[index]
 
     return points_new, index2
@@ -76,31 +76,106 @@ def prp_2_oh_array(arr):
     oh_arr = np.eye(arr_size)[arr_max]  # OH矩阵
     return oh_arr
 
-if __name__ == '__main__':
+def labeled_data():
     root = '/home/hwq/dataset/raildatarename'
     save_dir = '/home/hwq/dataset/labeled'
+    data_root = '/home/hwq/dataset/labeled/data'
+    label_root = '/home/hwq/dataset/labeled/label'
+    data_txt = '/home/hwq/dataset/labeled/datanpy'
+
+
     files = os.listdir(root)
     files.sort()
     print(len(files))
-    pcl_name='test1'
-    range = [-5.5, -1, -1, 1, 6.5, 70]
+    range = [-5.5, -1, -1, 1, 6.5, 50]
 
     for file in files:
-
+        file_name = file.split('.')[0]
         pcd = o3d.io.read_point_cloud(os.path.join(root,file))
-
         points = np.asarray(pcd.points)
         points_new,index = pc_reduce(points,range)
-        num_points = points_new.shape[0]
-        colors = pc_colors(index)
-        pcd.colors = o3d.utility.Vector3dVector(colors)
-        print(colors.shape)
-        pcd_new = o3d.geometry.PointCloud()
-        pcd_new.points = o3d.utility.Vector3dVector(points_new)
-        pcd_new.colors = o3d.utility.Vector3dVector(colors)
-        # pcd.paint_uniform_color([0.5, 0.5, 0.5])
-        # pcd.colors[1500] = [1, 0, 0]
-        # o3d.io.write_point_cloud(os.path.join(save_dir,file), pcd_new)
-        o3d.visualization.draw_geometries([pcd],window_name='ranged',
-                                      width=800,height=600)
+        new_data = np.concatenate((points,index[:, np.newaxis]),axis=-1)
+        np.save(os.path.join(data_txt,file_name),new_data)
         exit()
+def sort_train_test():
+    root = '/home/hwq/dataset/raildatarename'
+    save_dir = '/home/hwq/dataset/labeled'
+
+    import random
+
+    files = os.listdir(root)
+    files.sort()
+    print(len(files))
+    index_list = list(range(len(files)))
+    train_list = random.sample(index_list,int(0.75*len(files)))
+    train_list.sort()
+    print(len(train_list),(train_list))
+    test_list = list(set(index_list).difference(set(train_list)))
+    test_list.sort()
+    print(len(test_list),(test_list))
+
+    train_list_np = np.asarray(train_list).astype(np.int)
+    test_list_np = np.asarray(test_list).astype(np.int)
+    # np.save(save_dir+'/trainindex',train_list_np)
+
+    # np.save(save_dir+'/testindex',test_list_np)
+
+    train_npy = np.load(save_dir+'/trainindex.npy')
+    print('train list:',train_npy, len(train_npy))
+
+if __name__ == '__main__':
+    root = '/home/hwq/dataset/raildatarename'
+    save_dir = '/home/hwq/dataset/labeled'
+    data_root = '/home/hwq/dataset/labeled/data'
+    label_root = '/home/hwq/dataset/labeled/label'
+    data_txt = '/home/hwq/dataset/labeled/datanpy'
+
+    import random
+
+
+
+    # print('list',index_list)
+    files = os.listdir(root)
+    files.sort()
+    fns = sorted(os.listdir(root))
+    # fns.sort()
+    print('fns',len(fns),fns,)
+
+    # for file in files:
+    #     file_name = file.split('.')[0]
+    #     file_npy = file_name+'.npy'
+    #     # file_txt = file_name+'.txt'
+    #     # print(file_txt)
+    #     pcd = o3d.io.read_point_cloud(os.path.join(root,file))
+    #
+    #     points = np.asarray(pcd.points)
+    #
+    #     points_new,index = pc_reduce(points,range)
+    #     new_data = np.concatenate((points,index[:, np.newaxis]),axis=-1)
+    #     # print(new_data[:10])
+    #     # np.save(os.path.join(data_txt,file_name),new_data)
+    #     data_npy = np.load(os.path.join(data_txt,file_npy))
+    #     # print(data_npy[:10])
+    #     # exit()
+    #
+    #
+    #     # print('index',type(index),index.shape)
+    #     # # print(new_data[16000:16020])
+    #     # # exit()
+    #     colors_list = data_npy[:,-1].astype(int)
+    #     # num_points = points_new.shape[0]
+    #     colors = pc_colors(colors_list)
+    #     # pcd.colors = o3d.utility.Vector3dVector(colors)
+    #     # print(colors.shape)
+    #     pcd_new = o3d.geometry.PointCloud()
+    #     pcd_new.points = o3d.utility.Vector3dVector(data_npy[:,:3])
+    #
+    #
+    #
+    #     pcd_new.colors = o3d.utility.Vector3dVector(colors)
+    #     # pcd.paint_uniform_color([0.5, 0.5, 0.5])
+    #     # pcd.colors[1500] = [1, 0, 0]
+    #     # o3d.io.write_point_cloud(os.path.join(save_dir,file), pcd_new)
+    #     o3d.visualization.draw_geometries([pcd_new],window_name='ranged',
+    #                                   width=800,height=600)
+    #     exit()
